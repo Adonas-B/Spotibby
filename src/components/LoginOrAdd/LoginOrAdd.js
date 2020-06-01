f0729dc534fdd2596cbd93a165c134d831fdd47e
Aidan <aidanbridge@gmail.com>
2020-06-02 00:07:19 +0100

    Still trying to get Spotify working

--- src/components/LoginOrAdd/LoginOrAdd.js
+++ src/components/LoginOrAdd/LoginOrAdd.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import Cookies from 'js-cookie'
 import SpotifyLogin from 'react-spotify-login';
 
-const APP_URL = process.env.REACT_APP_URL
+const REACT_APP_URL = process.env.REACT_APP_URL
 
 const ButtonContainer = styled.div`
     display: flex;
@@ -14,7 +14,7 @@ const ButtonContainer = styled.div`
 export default function LoginOrAdd(props) {
     const [hasToken, setHasToken] = useState(false)
     const client_id = '1f044393f8714a7b9b9ab87a4777622c';
-    const redirect_uri = `${APP_URL}`;
+    const redirect_uri = `${REACT_APP_URL}/success`;
     const scopes = 'playlist-modify-public user-library-read';
 
     const { handleAddClick, spotifyPlaylistId } = props
